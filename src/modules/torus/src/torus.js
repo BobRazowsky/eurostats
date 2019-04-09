bf52073d4a1f5c56066d49714508ec1db5819441
ldemicheli@wanadev.fr <ldemicheli@wanadev.fr>
2019-04-09 14:17:34 +0200

    refacto module name

---
+++ src/modules/torus/src/torus.js
@@ -0,0 +1,54 @@
+import * as BABYLON from "@babylonjs/core/Legacy/legacy";
+import self from "../index";
+
+
+/**
+ * Simple Torus with changing color and orientation
+ */
+const Torus = {
+
+    /**
+     * @type BABYLON.Scene
+     */
+    scene: self.app.modules.babylonEngine.scene,
+
+    mesh: null,
+
+    init() {
+        if (self.app.modules.babylonEngine.isReady) {
+            this.createTorus();
+        }
+    },
+
+    createTorus() {
+        // The torus and its material
+        const torus = BABYLON.MeshBuilder.CreateTorus(
+            "torus", {
+                thickness: 0.5,
+                tessellation: 32,
+            },
+            Torus.scene
+        );
+        const torusMaterial = new BABYLON.StandardMaterial("torusMaterial", Torus.scene);
+        torus.material = torusMaterial;
+
+        // Torus update
+        Torus.scene.registerBeforeRender(() => {
+            // Color change
+            const now = Date.now();
+            torus.material.diffuseColor.r = (Math.sin(now * 0.0002)) ** 2;
+            torus.material.diffuseColor.g = (Math.cos(now * 0.0007)) ** 2;
+            torus.material.diffuseColor.b = (Math.cos(now * 0.0001)) ** 4;
+
+            // Rotation change
+            const ratio = Torus.scene.getAnimationRatio();
+            torus.rotate(BABYLON.Vector3.Left(), 0.01 * ratio);
+            torus.rotate(BABYLON.Vector3.Up(), 0.01 * ratio);
+        });
+
+        Torus.mesh = torus;
+    },
+
+
+};
+export default Torus;
---
+++ src/modules/torus/src/torus.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>Rouge : {{color.r}}</div>
+
+</template>
+
+<script>
+
+import Torus from "./torus";
+
+
+export default {
+    name: 'torus-infos',
+    data : () =>{
+        return {
+            mesh : Torus.mesh,
+            color : Torus.mesh.material.diffuseColor
+        }
+    },
+    components: {},
+    methods: {
+
+    }
+  }
+
+</script>
