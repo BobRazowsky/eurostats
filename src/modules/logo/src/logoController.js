fcaa584f2ed1247992c954bee3c11fa21c1e55c7
Bob Razowsky <aymeric.righyni@gmail.com>
2019-10-27 22:57:12 +0100

    better

--- src/modules/logo/src/logoController.js
+++ src/modules/logo/src/logoController.js
@@ -5,9 +5,14 @@ export default class LogoController {
     constructor() {
         let results = [];
         let tirageNbOnly = [];
+        
+        this.lastTirage = [];
+        this.lastStars = [];
+        this.allData = null;
         this.getJSON("assets/data/results.json", (data) => {
-            console.log(data);
+            this.allData = data;
             for (let i = 0; i < data.length; i++) {
+                let stars = [];
                 let tirage = data[i];
                 let tirageRes = [];
                 let nbOnly = [];
@@ -15,19 +20,24 @@ export default class LogoController {
                     tirageRes.push(tirage["boule_" + j]);
                     nbOnly.push(tirage["boule_" + j]);
                 }
+                if(i === 0) {
+                    this.lastTirage = nbOnly;
+                }
                 tirageNbOnly.push(nbOnly.sort((a, b) => a - b));
                 for (let k = 1; k < 3; k++) {
                     tirageRes.push(tirage["etoile_" + k]);
+                    stars.push(tirage["etoile_" + k]);
                 }
-
-
+                if(i === 0) {
+                    this.lastStars = stars;
+                }
 
                 results.push(tirageRes);
             }
-
-            console.log(results);
-            this.buildInfos(results.length);
+            this.nbDraws = results.length;
+            //this.buildInfos(results.length);
             this.buildData(results);
+            this.buildLastDraw(results[0]);
             this.findSimilarity(tirageNbOnly);
         });
     }
@@ -37,7 +47,6 @@ export default class LogoController {
         request.open('GET', url, true);
         request.onload = function(){
             if(request.status >= 200 && request.status < 400){
-                console.log(JSON.parse(request.responseText));
                 var data = JSON.parse(request.responseText);
                 callback(data);
             }
@@ -81,20 +90,17 @@ export default class LogoController {
             }
         }
 
-        console.log(nbData, starData);
         this.findSimilarity(nbData);
         this.buildChart(nbData, starData);
     }
 
     findSimilarity(data) {
-        console.log("DATA", data);
         for (let i = 0; i < data.length; i++) {
             let dat = data[i].join("");
-            //console.log(dat);
             for(let j = 0; j < data.length; j++) {
                 if(j == i) continue;
                 if(dat == data[j].join("")) {
-                    console.log("SIMILAR", data[i], i, data[j], j);
+                    console.log("SIMILAR", data[i], this.allData[i].date_de_tirage, data[j], this.allData[j].date_de_tirage);
                 }
             }
         }
@@ -107,9 +113,11 @@ export default class LogoController {
             data: {
                 labels: Object.keys(nbData),
                 datasets: [{
-                    label: "Numéros",
+                    label: "Numéros - présence pour " + this.nbDraws + " tirages",
                     data: Object.values(nbData),
-                    backgroundColor: this.getColors(Object.values(nbData))
+                    backgroundColor: this.getColors(nbData, 'numbers'),
+                    borderColor: this.getBorderColor(nbData, 'numbers'),
+                    borderWidth: this.getBorderWidth(nbData, 'numbers')
                 }]
             },
             options: {
@@ -130,9 +138,11 @@ export default class LogoController {
             data: {
                 labels: Object.keys(starData),
                 datasets: [{
-                    label: "Etoiles",
+                    label: "Etoiles - présence pour " + this.nbDraws + " tirages",
                     data: Object.values(starData),
-                    backgroundColor: this.getColors(Object.values(starData))
+                    backgroundColor: this.getColors(starData, "stars"),
+                    borderColor: this.getBorderColor(starData, 'stars'),
+                    borderWidth: this.getBorderWidth(starData, 'stars')
                 }]
             },
             options: {
@@ -153,18 +163,103 @@ export default class LogoController {
         infoContainer.innerHTML = length;
     }
 
-    getColors(data) {
-        let colors = [];
-        console.log(data);
-        let max = Math.max(...data);
-        console.log(max);
+    buildLastDraw(lastDraw) {
+        let container = document.getElementById("draw");
+        console.log(lastDraw);
 
-        for (let i = 0; i < data.length; i++) {
-            colors.push('rgba(0, ' + ((data[i] * 255) / max) + ', 0, 1)');
+        for(let i = 0; i < 5; i++) {
+            let nb = document.createElement("div");
+            nb.innerHTML = lastDraw[i];
+            nb.classList.add("number");
+            container.appendChild(nb);
+        }
+
+        for(let j = 5; j < 7; j++) {
+            let star = document.createElement("div");
+            star.innerHTML = lastDraw[j];
+            star.classList.add("star");
+            container.appendChild(star);
+        }
+    }
+
+    getColors(data, type) {
+        let colors = [];
+
+        for(var nb in data) {
+            if(type == "numbers") {
+                let color = 'rgba(86, 105, 154, 1)';
+                // for (let j = 0; j < this.lastTirage.length; j++) {
+                //     if(nb == this.lastTirage[j]) {
+                //         console.log(this.lastTirage);
+                //         color = 'rgba(0, 50, 155, 1)';
+                //     }
+                // }
+                colors.push(color);
+            } else if(type == "stars") {
+                let color = 'rgba(253, 209, 116, 1)';
+                // for (let j = 0; j < this.lastStars.length; j++) {
+                //     if(nb == this.lastStars[j]) {
+                //         console.log(this.lastStars);
+                //         color = 'rgba(246, 178, 32, 1)';
+                //     }
+                // }
+                colors.push(color);
+            }
         }
 
         return colors;
+    }
 
+    getBorderColor(data, type) {
+        let borderColors = [];
+
+        for(var nb in data) {
+            if(type == "numbers") {
+                let color = undefined;
+                for (let j = 0; j < this.lastTirage.length; j++) {
+                    if(nb == this.lastTirage[j]) {
+                        color = 'rgba(0, 0, 0, 1)';
+                    }
+                }
+                borderColors.push(color);
+            } else if(type == "stars") {
+                let color = undefined;
+                for (let j = 0; j < this.lastStars.length; j++) {
+                    if(nb == this.lastStars[j]) {
+                        color = 'rgba(0, 0, 0, 1)';
+                    }
+                }
+                borderColors.push(color);
+            }
+        }
+
+        return borderColors;     
+    }
+
+    getBorderWidth(data, type) {
+        let borderWidths = [];
+
+        for(var nb in data) {
+            if(type == "numbers") {
+                let width = undefined;
+                for (let j = 0; j < this.lastTirage.length; j++) {
+                    if(nb == this.lastTirage[j]) {
+                        width = 2;
+                    }
+                }
+                borderWidths.push(width);
+            } else if(type == "stars") {
+                let width = undefined;
+                for (let j = 0; j < this.lastStars.length; j++) {
+                    if(nb == this.lastStars[j]) {
+                        width = 2;
+                    }
+                }
+                borderWidths.push(width);
+            }
+        }
+
+        return borderWidths;     
     }
 
 }
